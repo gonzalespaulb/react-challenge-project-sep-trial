90ffb771d1a1e2bf44a0a41d31d11395f156ec2e
Paul Gonzales <paulgonzales@MBA.local>
2021-09-14 00:30:35 -0700

    issue 2 time padding fixed

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -10,6 +10,15 @@ const OrdersList = (props) => {
 
     return orders.map(order => {
         const createdDate = new Date(order.createdAt);
+
+        const produceOrderTime = () => {
+            const hours = createdDate.getHours();
+            const minutes = createdDate.getMinutes() < 10 ? `0${createdDate.getMinutes()}` : createdDate.getMinutes();
+            const seconds = createdDate.getSeconds() < 10 ? `0${createdDate.getSeconds()}` : createdDate.getSeconds();
+            const orderTime = `${hours}:${minutes}:${seconds}`;
+            return orderTime;
+        }
+
         return (
             <div className="row view-order-container" key={order._id}>
                 <div className="col-md-4 view-order-left-col p-3">
@@ -17,7 +26,7 @@ const OrdersList = (props) => {
                     <p>Ordered by: {order.ordered_by || ''}</p>
                 </div>
                 <div className="col-md-4 d-flex view-order-middle-col">
-                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
+                    <p>Order placed at {produceOrderTime()}</p>
                     <p>Quantity: {order.quantity}</p>
                 </div>
                 <div className="col-md-4 view-order-right-col">
