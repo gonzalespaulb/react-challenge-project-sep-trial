d8880367db7dc49a0165f875cad6590b29dc261a
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2021-09-23 23:23:04 -0700

    Issue 10 User Email Displayed

--- application/src/components/common/template.js
+++ application/src/components/common/template.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Nav } from '../../components';
 import './template.css';
+import { useSelector } from 'react-redux';
+
 
 const Template = props => {
+
+    const auth = useSelector((state) => state.auth);
+
     return (
         <div className="bg-layer">
             <div className="fg-layer">
+                <div className="username-display"><h4>{auth.email}</h4></div>
                 <label className="logo">Bruce's Diner</label>
                 <Nav />
                 {props.children}
